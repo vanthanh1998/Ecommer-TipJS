6d88f2650a1386ab28a392d61cd1cd3ad64184f4
Văn Thanh <vanthanh@jv-it.com.vn>
2023-12-28 23:21:47 +0700

    handdle err

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -4,16 +4,7 @@ const AccessService = require("../services/access.service")
 
 class AccessController {
     signUp = async (req, res, next) => {
-        try {
-            /*
-                200: ok
-                201: created
-            */
-            console.log(`[P]::SignUp::`, req.body)
-            return res.status(200).json(await AccessService.signUp(req.body))
-        } catch (error) {
-            next(error)
-        }
+        return res.status(200).json(await AccessService.signUp(req.body))
     }
 }
 
